6fbfe57e94d7cbe130ef14e0cae78d13be0dec8c
nareshmaharaj <naresh99369@gmail.com>
2018-12-19 03:22:21 +0000

    creates strings

---
+++ DataTypes/strings/Rule Of Creating Variables.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# Creating Variables in python
+
+# Rule of creating variable in python
+
+'''
+1. A-Z
+2. a-z
+3. 0-9
+4. _
+5. A-Za-z
+6.
+'''
+
+# creating variable in python
+firstname = 'naresh'
+middlename = 'kumar'
+lastname = 'maharaj'
+_language = 'python'
+varsion_number = 3.7
+_007 = 'james bond'
+
+print(firstname, middlename,lastname,_language,varsion_number,_007)
+
+
+
+
+
+
+
---
+++ DataTypes/strings/strings_method.py
@@ -0,0 +1,52 @@
+"""
+1.capitalize() method:
+2.center() method:
+3.count() method:
+4.startswith() method:
+5.endswith() method:
+"""
+
+name = "naresh kumar maharaj"
+
+print(name.capitalize())
+
+print(type(name),len(name),id(name))
+
+print("center method: ", name.center(24,'@'))
+
+print(name.count('a',0,20))
+
+print(name.capitalize())
+print(type(name),len(name),id(name))
+
+print(name.count('a'))
+
+print(name.startswith('naresh',0,20))
+print(name.endswith('maharaj',0,20))
+
+print(name.startswith('naresh',0,20))
+print(name.endswith('maharaj',0,20))
+print(type(name),len(name),id(name))
+
+firstname = 'naresh'
+lastname = 'maharaj'
+print(name.find('maharaj',0,17))
+print(name.index('maharaj'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
