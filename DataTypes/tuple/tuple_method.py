044e583a2592bb97c861e3b3bbe28e94387329ea
nareshmaharaj <naresh99369@gmail.com>
2018-12-21 20:28:05 +0000

    tuples

---
+++ DataTypes/tuple/tuple_method.py
@@ -0,0 +1,33 @@
+
+tup1 = ("aws",'azure',1997,2947, 50.75)
+
+tup2 = (1, 2, 3, 4, 5, 6, 7)
+
+print(tuple(enumerate(tup1)),type(tup1),id(tup1),len(tup1))
+print(tuple(enumerate(tup2)),type(tup2),id(tup2),len(tup2))
+
+print(tup1[3])
+print(tup1[:5])
+
+print(tup1)
+
+tup = (1,2, [1,3])
+print(tuple(enumerate(tup)),type(tup))
+print(tup,type(tup),tuple(enumerate(tup)))
+print(tuple(enumerate(tup)),type(tup))
+
+
+tup = (5, 6, [1,3])
+print(tuple(enumerate(tup)),type(tup))
+
+
+
+
+
+
+
+
+
+
+
+
