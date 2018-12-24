a561151a7fb2a8a80b0c570aff11cd542c8174f6
nareshmaharaj <naresh99369@gmail.com>
2018-12-24 19:45:40 +0000

    tuples new program and list

--- DataTypes/list/List-Notes.py
+++ DataTypes/list/List-Notes.py
@@ -1,8 +1,40 @@
 
+num = [1, 2, 3, 4, 5, 6]
+print(sum(num))
 
+courses = ['physics', 'chem', 'math','history']
+num = [1, 2, 3, 4, 5, 6]
+print(courses[1])
+print(num[2])
 
+courses = ['physics', 'chem', 'math','history']
+for course in courses:
+    print(course)
 
+courses = ['physics', 'chem', 'math','history']
+for index, course in enumerate(courses,start=1):
+    print(index,course)
 
+courses = ['physics', 'chem', 'math','history']
+course_str = ','.join(courses)
+print(course_str)
+
+courses = ['physics', 'chem', 'math','history']
+course_str = '-'.join(courses)
+print(course_str)
+
+courses = ['physics', 'chem', 'math','history']
+new_list = course_str.split('-')
+print(new_list)
+
+courses1 = ['physics', 'chem', 'math','history']
+course2 = courses1
+print(courses1)
+print(course2)
+
+courses1[0] = 'art'
+print(courses1)
+print(course2)
 
 
 
--- DataTypes/list/list-method.py
+++ DataTypes/list/list-method.py
@@ -1,22 +1,22 @@
 
-list_1 = [1, 2, 3]
-print(list_1,type(list_1),id(list_1),len(list_1))
 
-list_1.append(4,)
-print(list_1,type(list_1),id(list_1),len(list_1))
-list_1.append("hello naresh")
-print(list_1,type(list_1),id(list_1),len(list_1))
 
-list_x =["naresh","madhu",1947,1988,"raman"]
-list_y =[22, 34, 54, 66, 78, 99]
 
-print(list_x.count("madhu"))
-print(list_y.count(54))
 
-list_x =["naresh","madhu",1947,1988,"raman"]
-list_y =[22, 34, 54, 66, 78, 99]
-list_x.extend(list_y)
-print(list_x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
